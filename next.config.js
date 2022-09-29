ec0fb01af40a93ffb7e7e5681694196727abf5af
techswarn <tech.swarns@gmail.com>
2022-09-29 16:57:37 +0530

    code update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-            domains: ['backendapp.fra1.digitaloceanspaces.com']
+            domains: ['backendapp.fra1.digitaloceanspaces.com'],
+            unoptimized: true
     },
 }
     
