1d6541c866ca25bcf6d8e8d2550198b8d2d05bd4
techswarn <tech.swarns@gmail.com>
2023-02-08 15:08:52 +0530

    code update

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,7 +1,20 @@
+import Link from 'next/link'
+
 const Footer = () => {
     return (
-      <footer>
-        Copyright 2021
+      <footer className="footer">
+        <div className="container grid grid--2-cols">
+            <div>
+              <p className="copyright">
+                Copyright &copy; 2027 by Omnifood, Inc. All rights reserved.
+              </p>
+            </div>
+            <div className="footer-links">
+              <Link className="footer-link" href="/home">FAQ</Link>
+              <Link className="footer-link" href="/privacy-policy">Privacy & Policy</Link>
+              <Link className="footer-link" href="/terms-and-conditions">Terms and Conditions</Link>
+            </div>
+        </div>
       </footer>
     );
   }
---
+++ pages/privacy-policy.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+const Privacy = () => {
+  return (
+    <>
+      <Head>
+        <title>Privacy policy</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1>Privacy</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloribus distinctio fugiat sint explicabo excepturi reprehenderit saepe dicta esse exercitationem. Eum.
+      </div>
+    </>
+  );
+}
+ 
+export default Privacy;
