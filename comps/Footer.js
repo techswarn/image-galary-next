b7f5d0a6abe410649dd76832623b8b5132cd6313
techswarn <tech.swarns@gmail.com>
2023-02-09 04:00:32 +0530

    code update

--- comps/Footer.js
+++ comps/Footer.js
@@ -6,11 +6,10 @@ const Footer = () => {
         <div className="container grid grid--2-cols">
             <div>
               <p className="copyright">
-                Copyright &copy; 2027 by Omnifood, Inc. All rights reserved.
+                Copyright &copy; All rights reserved.
               </p>
             </div>
             <div className="footer-links">
-              <Link className="footer-link" href="/home">FAQ</Link>
               <Link className="footer-link" href="/privacy-policy">Privacy & Policy</Link>
               <Link className="footer-link" href="/terms-and-conditions">Terms and Conditions</Link>
             </div>
