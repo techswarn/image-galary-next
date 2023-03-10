b2ef8242413a96427e585489383ca3aea7478fc3
techswarn <tech.swarns@gmail.com>
2023-03-10 16:40:43 +0530

    code update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+          {
+            source: '/:all*(svg|jpg|png)',
+            locale: false,
+            headers: [
+              {
+                key: 'Cache-Control',
+                value: 'public, max-age=9999999999, must-revalidate',
+              }
+            ],
+          },
+        ]
+      },
     images: {
             domains: ['backendapp.fra1.digitaloceanspaces.com'],
             loader: 'akamai',
