efccef736e187826289d7ca27edb0b3b64ab6058
techswarn <tech.swarns@gmail.com>
2023-03-11 13:11:46 +0530

    code update

--- next.config.js
+++ next.config.js
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/office",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "s-maxage=1, stale-while-revalidate=59",
-          },
-        ],
-      },
-    ];
-  },
 
     images: {
             domains: ['backendapp.fra1.digitaloceanspaces.com'],
