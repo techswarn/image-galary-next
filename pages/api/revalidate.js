b56f389e321fe8bd12df47bdf6c780ee7f9554df
techswarn <tech.swarns@gmail.com>
2023-03-30 13:09:41 +0530

    made changes for On demand revalidation

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
       console.log("-------------------------------****--------------------------------")
       // this should be the actual path not a rewritten path
       // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-      await res.revalidate('/office')
+      await res.revalidate('/index')
       return res.json({ revalidated: true })
     } catch (err) {
         console.log(err)
