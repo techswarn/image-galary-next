233787e721cd0d6b4c7af72f4ba7f71aa6b99e74
techswarn <tech.swarns@gmail.com>
2023-03-30 13:13:53 +0530

    revalidate path

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
       console.log("-------------------------------****--------------------------------")
       // this should be the actual path not a rewritten path
       // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-      await res.revalidate('/index')
+      await res.revalidate('/')
       return res.json({ revalidated: true })
     } catch (err) {
         console.log(err)
