7b44e8198ef241a141f8b7250a20cb40bdf8be26
techswarn <tech.swarns@gmail.com>
2023-06-22 13:48:11 +0530

    code update

--- next.config.js
+++ next.config.js
@@ -1,19 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        // Defaults to 50MB
-        isrMemoryCacheSize: 0,
-    },
+  experimental: {
+    // Defaults to 50MB
+    isrMemoryCacheSize: 0,
+  },
+  output: "export",
+  distDir: "_static",
+  images: {
+    unoptimized: true,
+  },
 
-// //     images: {
-// //             domains: ['backendapp.fra1.digitaloceanspaces.com', 'localhost'],
-// //             loader: 'akamai',
-// //             path: '',        
-// // //            unoptimized: true
-//     },
-//     compress: true,
+  // //     images: {
+  // //             domains: ['backendapp.fra1.digitaloceanspaces.com', 'localhost'],
+  // //             loader: 'akamai',
+  // //             path: '',
+  // // //            unoptimized: true
+  //     },
+  //     compress: true,
 
-//    trailingSlash: true,  //Use this only if you deploy Nextjs as a static site. 
-}
+  //    trailingSlash: true,  //Use this only if you deploy Nextjs as a static site.
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
