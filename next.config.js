d7a9eb24bdbc2fe8d96e4e0ffdcf98b286317000
techswarn <tech.swarns@gmail.com>
2023-10-31 13:03:31 +0530

    code update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["backendapp.fra1.digitaloceanspaces.com"],
-    loader: "akamai",
-    path: "",
-    //            unoptimized: true
-  },
+  // images: {
+  //   domains: ["backendapp.fra1.digitaloceanspaces.com"],
+  //   loader: "akamai",
+  //   path: "",
+  //   //            unoptimized: true
+  // },
   compress: true,
 
   trailingSlash: true,
--- pages/about.js
+++ pages/about.js
@@ -1,20 +1,27 @@
-import Head from 'next/head'
-import {signIn} from "next-auth/react"
-
+import Head from "next/head";
+import Image from "next/image";
+import { signIn } from "next-auth/react";
+import sampleimage from "./../public/images/sample.jpg";
 const About = () => {
   return (
     <>
       <Head>
         <title>Ninja List | About</title>
-        <meta name="keywords" content="ninjas"/>
+        <meta name="keywords" content="ninjas" />
       </Head>
       <div className="section-margin">
         <h1>About</h1>
         <p>Just a CRUD Nextjs APP</p>
+        <Image
+          src={sampleimage}
+          width={500}
+          height={500}
+          alt="Picture of the author"
+        />
         <button onClick={() => signIn()}>Login</button>
       </div>
     </>
   );
-}
- 
+};
+
 export default About;
